a82dc1d6ef54b811de5eb9f0c440a36ab56c991b
EdinaAdzem <71989436+EdinaAdzem@users.noreply.github.com>
2024-09-10 14:19:16 +0200

    add authors route to the step 4, creating and building the library

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from data_models import db, Author, Book
 
@@ -17,6 +17,30 @@ db.init_app(app)
 # Create once
 #with app.app_context():
     #db.create_all()
+
+
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+    if request.method == 'POST':
+        # Get data out of the form
+        name = request.form['name']
+        birth_date = request.form.get('birth_date')
+        date_of_death = request.form.get('date_of_death')
+
+        # auth obj
+        new_author = Author(name=name, birth_date=birth_date, date_of_death=date_of_death)
+
+        try:
+            # Add the new author to the database
+            db.session.add(new_author)
+            db.session.commit()
+            flash('Author added successfully!', 'success')
+            return redirect(url_for('add_author'))
+        except Exception as e:
+            db.session.rollback()  # roll back in case of issues like sevice down
+            flash(f'Author could not be added, please check the services: {e}', 'danger')
+
+    return render_template('add_author.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
